78e33d8718a24f1b5bd0da1e6bde36a904f02c10
natalievalcin <92767163+natalievalcin@users.noreply.github.com>
2023-02-08 14:47:35 -0500

    Was trying to work on something but changed mind

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,24 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MatSelectModule } from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectComponent } from './components/select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    HeaderComponent
+    HeaderComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
     LeafletModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatSelectModule
   ],
   providers: [],
