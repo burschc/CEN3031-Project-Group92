6d539a17ce0cc9f2c36a44e9d4896c88ff550db3
burschc <christopherjbursch@gmail.com>
2023-02-13 01:20:25 -0500

    a start

--- api/go.mod
+++ api/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
 )
--- api/platform/main.go
+++ api/platform/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	"github.com/pkg/browser"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"ufpmp/httpd/sprint1"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/browser"
 )
 
 func main() {
 	//Run a command to create a new window using the system's default browser.
+	database, _ := sql.Open("sqlite3", "./nraboy.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement.Exec("Nic", "Raboy")
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+
 	err := browser.OpenURL("http://localhost:8080/sprint1")
 	if err != nil {
 		log.Print(err)
