51dd1940ca875a18526bb985ca2e424fe4a69c4e
burschc <christopherjbursch@gmail.com>
2023-02-27 21:36:46 -0500

    setup rough username/password/passtype db

--- api/platform/main.go
+++ api/platform/main.go
@@ -17,18 +17,19 @@ import (
 
 func main() {
 	//Run a command to create a new window using the system's default browser.
-	database, _ := sql.Open("sqlite3", "./nraboy.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	database, _ := sql.Open("sqlite3", "./testdb.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, passtype INTEGER)")
 	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	statement.Exec("Nic", "Raboy")
-	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	statement, _ = database.Prepare("INSERT INTO users (username, password, passtype) VALUES (?, ?, ?)")
+	statement.Exec("TestUser", "TestPassword", 1)
+	rows, _ := database.Query("SELECT id, username, password, passtype FROM users")
 	var id int
-	var firstname string
-	var lastname string
+	var username string
+	var password string
+	var passtype int
 	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+		rows.Scan(&id, &username, &password, &passtype)
+		fmt.Println(strconv.Itoa(id) + ": " + username + " " + password + " " + strconv.Itoa(passtype))
 	}
 
 	err := browser.OpenURL("http://localhost:8080/sprint1")
