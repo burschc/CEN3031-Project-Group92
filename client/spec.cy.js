a3c414b970a6500c93e6de36cc474ef564c126ab
sbarthelemy01 <sbarthelemy@ufl.edu>
2023-03-01 18:44:55 -0500

    started cypress test

---
+++ client/spec.cy.js
@@ -0,0 +1,13 @@
+describe('Parking App Test', () => {
+  it('Will apply one of the flters to the map', () => {
+    
+    cy.visit('localhost:4200')
+    
+    cy.get('App-Select').click()
+    //cy.contains('Decals').click()
+
+    //cy.visit('localhost:4200/api/filter/decals') //populates the dropdown menu with an array of decals
+    //localhost:4200/api/filter/decal/{"Brown3"} //filters out the options and returns those in Brown3
+
+  })
+})
